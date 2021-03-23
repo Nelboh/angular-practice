49db8446e1027f4a76ed55ed6887651779990dcd
Eleanor Hobbs <eleanor.hobbs@activequote.com>
2021-03-23 13:39:08 +0000

    Practiced event binding with simple add server button functionality

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = "No server was created!";
 
   constructor() { 
     setTimeout(() => {
@@ -15,7 +16,16 @@ export class ServersComponent implements OnInit {
     }, 2000);
   }
 
-  ngOnInit(): void {
+  // ngOnInit(): void {
+  // }
+
+  ngOnInit() {
+
   }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server created";
+  }
+
 
 }
